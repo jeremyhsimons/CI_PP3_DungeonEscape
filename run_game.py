98514d45104c355b245bd1582a224c285ce153e4
jeremyhsimons <jeremysimons15@gmail.com>
2023-02-28 17:34:10 +0000

    create placeholder code for room method to generate room in terminal.

--- run_game.py
+++ run_game.py
@@ -1,1 +1,9 @@
+# Code to demonstrate how to make a game board/map using a list of lists.
+big_list = [
+    ['1', '2', '3', '4', '5'], ['1', '.', '3', '4', '5'],
+    ['1', '2', '3', '4', '5']
+    ] 
 
+for i in big_list:
+    x = " ".join(i)
+    print(x)
