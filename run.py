b02c5bc61ebf845cb4bbbf6b92415518179edeae
jeremyhsimons <jeremysimons15@gmail.com>
2023-02-28 16:06:43 +0000

    Add code to check if the user's login details are valid.

--- run.py
+++ run.py
@@ -2,11 +2,28 @@ from sheet_data import get_logins
 
 
 def login():
+    """
+    Calls get_logins function and checks if the user's
+    inputs match a valid username/password.
+    """
+    logins = get_logins()
     print('Please enter your username and password')
-    username = input('\n Username: ')
-    password = input('\n Password: ')
-    print(username)
-    print(password)
+    uname = input('\n Username: ')
+    pword = input('\n Password: ')
+    logins_checked = 0
+    for i in logins:
+        if uname == i['Username']:
+            print('\nValid user.')
+            if pword == i['Password']:
+                print('login successful! Welcome!')
+            else:
+                print('Incorrect password. Please try again.\n')
+                login()
+        else:
+            logins_checked += 1
+    if logins_checked == len(logins):
+        print('Sorry, this user does not exist. Please try again.\n')
+        login()
 
 
 login()
