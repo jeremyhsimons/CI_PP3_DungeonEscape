f6b799cecb39ba92638033b8adb6ce93bce67b90
jeremyhsimons <jeremysimons15@gmail.com>
2023-03-01 13:36:46 +0000

    Create classes for the player, doors, and rooms that will feature in the game.

--- run_game.py
+++ run_game.py
@@ -7,3 +7,36 @@ big_list = [
 for i in big_list:
     x = " ".join(i)
     print(x)
+
+
+class Player:
+    """
+    A class that defines the common features of users/players
+    in-game characters.
+    """
+    def __init__(self, name, lives, points, keys):
+        self.name = name
+        self.lives = lives
+        self.points = points
+        self.keys = keys
+
+
+class Door:
+    """
+    A class that defines the common features of different
+    doors that the player may come across in the game.
+    """
+    def __init__(self, status, type, position):
+        self.status = status
+        self.type = type
+        self.position = position
+
+
+class Room:
+    """
+    A class that defines the common features of different
+    rooms that a player might discover in the game.
+    """
+    def __init__(self, doors, special_event):
+        self.doors = doors
+        self.special_event = special_event
