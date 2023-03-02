1d76e2a8d9665491829df4975143789c3adcc74d
jeremyhsimons <jeremysimons15@gmail.com>
2023-03-02 17:14:35 +0000

    Create function to generate levels and access their layouts to store in a game layout list.

--- run_game.py
+++ run_game.py
@@ -1,5 +1,7 @@
 import random
-
+GAME_LAYOUT = []
+LEVELS_DICT = {}
+LEVELS_PLAYED = 0
 
 class Player:
     """
@@ -19,25 +21,26 @@ class Level:
     will be presented to the player, and a level_number to keep
     track of how far the player has progressed.
     """
+    layouts = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
 
-    layouts = [
-        [["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"],
-        ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
-        ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
-        [" ", ".", ".", ".", ".", ".", ".", ".", ".", ".", " "],
-        ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
-        ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
-        ["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"]],
-        [["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"],
-        ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
-        ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
-        [" ", ".", ".", ".", ".", ".", ".", ".", ".", ".", " "],
-        ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
-        ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
-        ["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"]],
-    ]
+    #layouts = [
+    #   [["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"],
+    #    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
+    #    ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
+    #    [" ", ".", ".", ".", ".", ".", ".", ".", ".", ".", " "],
+    #    ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
+    #    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
+    #    ["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"]],
+    #    [["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"],
+    #    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
+    #    ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
+    #    [" ", ".", ".", ".", ".", ".", ".", ".", ".", ".", " "],
+    #    ["|", "-", "-", "-", "-", "-", "-", "-", "-", "-", "|"],
+    #    ["|", ".", ".", ".", ".", ".", ".", ".", ".", ".", "|"],
+    #    ["O", "-", "-", "-", "-", "-", "-", "-", "-", "-", "O"]],
+    #]
 
-    def __init__(self, layout_number, level_number):
+    def __init__(self, level_number, layout_number):
         self.layout_number = layout_number
         self.level_number = level_number
 
@@ -59,14 +62,36 @@ def start_game():
     """
     The function that controls the flow of the game
     """
-    random_layout_selector()
-    level = Level(0, 1)
+    level_order = random_layout_selector()
+    # GAME_LAYOUT = level_order
+    generate_levels(level_order)
+    print(GAME_LAYOUT)
+    print(LEVELS_DICT)
 
 
 def random_layout_selector():
+    """
+    A function to order the appearance of the different levels
+    (stored in the level class.)
+    """
     layout_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    print(layout_order)
     random.shuffle(layout_order)
-    print(layout_order)
+    return layout_order
+
+def generate_levels(data):
+    """
+    A function to create new instances of the Level class and return
+    a randomised list of the levels for the game.
+    """
+    this_game_layout = []
+    level_num = 0
+    for i in data:
+        level_num += 1
+        level = Level(level_num, i)
+        LEVELS_DICT.update({f"{level_num}" : i})
+        GAME_LAYOUT.append(level.layouts[i])
+        this_game_layout.append(level.layouts[i])
+    return this_game_layout
+
 
 start_game()
